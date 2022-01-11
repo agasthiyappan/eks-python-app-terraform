4837168621a59153307bf46a0224ae22973e2fea
agasthiyappan <agasthiyappan.a@onmobile.com>
2022-01-11 10:04:02 +0530

    Add files via upload

---
+++ helm/eks.tf
@@ -0,0 +1,7 @@
+provider "kubernetes" {
+  alias                  = "eks"
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  load_config_file       = false
+}
